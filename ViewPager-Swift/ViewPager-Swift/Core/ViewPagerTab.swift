73e3194bf6e5fee72311d18cd6e07e13a26972e4
Mano360 <48287504+Mano360@users.noreply.github.com>
2019-03-09 11:28:07 +0530

    Update ViewPagerTab.swift

--- ViewPager-Swift/ViewPager-Swift/Core/ViewPagerTab.swift
+++ ViewPager-Swift/ViewPager-Swift/Core/ViewPagerTab.swift
@@ -2,8 +2,8 @@
 //  ViewPagerTab.swift
 //  ViewPager-Swift
 //
-//  Created by Nishan on 1/9/17.
-//  Copyright © 2017 Nishan. All rights reserved.
+//  Created by Manohar on 2/03/19.
+//  Copyright © 2019 Omnicuris. All rights reserved.
 //
 
 import Foundation
